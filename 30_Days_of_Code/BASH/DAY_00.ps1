aa3fa5e930d18ac101b5c1ff5a982fe001c7ab55
kev225 <33672899+kev225@users.noreply.github.com>
2022-07-09 00:05:49 -0400

    Update DAY_00.ps1

--- 30_Days_of_Code/BASH/DAY_00.ps1
+++ 30_Days_of_Code/BASH/DAY_00.ps1
@@ -2,10 +2,6 @@
 #To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
 #and finally print the value of your variable on a second line.*/
 
-read inputString # get a line of input from stdin and save it to our variable
-
-# Your first line of output goes here
+read inputString
 echo 'Hello, World.'
-
-# Write the second line of output
 echo $inputString
